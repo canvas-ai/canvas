9c8193da7796645a075c14fcf7a851c39ddcab00
idnc_sk <me@idnc.sk>
2024-08-26 19:10:26 +0200

    git subtree qf

--- scripts/update-subtrees.sh
+++ scripts/update-subtrees.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-git subtree pull --prefix src/ui canvas-ui main --squash
-git subtree pull --prefix src/server canvas-server main --squash
+git subtree pull --prefix src/ui canvas-ai/canvas-electron main --squash
+git subtree pull --prefix src/server canvas-ai/canvas-server main --squash
